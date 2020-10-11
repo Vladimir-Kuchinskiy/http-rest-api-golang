cf1d139bd5fef0c42ec3d68ae5b0a0a9f6548ef9
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2020-10-11 12:49:45 +0300

    Add middlewares:  * authenticateUser  * setRequestID  * logRequest

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -3,10 +3,12 @@ package apiserver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 
 	"github.com/Vladimir-Kuchinskiy/http-rest-api-golang/internal/app/entity"
@@ -15,6 +17,50 @@ import (
 
 	"github.com/Vladimir-Kuchinskiy/http-rest-api-golang/internal/app/store/teststore"
 )
+
+func TestServer_AuthenticateUser(t *testing.T) {
+	store := teststore.New()
+	u := entity.TestUser(t)
+	store.User().Create(u)
+
+	testCases := []struct {
+		desc         string
+		cookieValue  map[interface{}]interface{}
+		expectedCode int
+	}{
+		{
+			desc: "when authenticated",
+			cookieValue: map[interface{}]interface{}{
+				"user_id": u.ID,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			desc:         "when not authenticated",
+			cookieValue:  nil,
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	secretKey := []byte("secret")
+	s := newServer(store, sessions.NewCookieStore(secretKey))
+	sc := securecookie.New(secretKey, nil)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			cookieStr, _ := sc.Encode(sessionName, tC.cookieValue)
+			req.Header.Set("Cookie", fmt.Sprintf("%s=%s", sessionName, cookieStr))
+			s.authenticateUser(handler).ServeHTTP(rec, req)
+
+			assert.Equal(t, tC.expectedCode, rec.Code)
+		})
+	}
+}
 
 func TestServer_HandleUsersCreate(t *testing.T) {
 	s := newServer(teststore.New(), sessions.NewCookieStore([]byte("secret")))
